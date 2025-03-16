352c355a69c61c50f7e4e1e2a05dfa2126e77e45
xZKuuoaObZ <xzkuuoaobz@dorenue.ru>
2025-03-16 21:37:51 +0300

    new protos

--- gen/service_grpc.pb.go
+++ gen/service_grpc.pb.go
@@ -19,101 +19,101 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	FileService_ProcessFile_FullMethodName = "/fileservice.FileService/ProcessFile"
+	ProcessingService_ProcessFile_FullMethodName = "/fileservice.ProcessingService/ProcessFile"
 )
 
-// FileServiceClient is the client API for FileService service.
+// ProcessingServiceClient is the client API for ProcessingService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type FileServiceClient interface {
-	ProcessFile(ctx context.Context, in *FileRequest, opts ...grpc.CallOption) (*FileResponse, error)
+type ProcessingServiceClient interface {
+	ProcessFile(ctx context.Context, in *ProcessRequest, opts ...grpc.CallOption) (*ProcessResponse, error)
 }
 
-type fileServiceClient struct {
+type processingServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewFileServiceClient(cc grpc.ClientConnInterface) FileServiceClient {
-	return &fileServiceClient{cc}
+func NewProcessingServiceClient(cc grpc.ClientConnInterface) ProcessingServiceClient {
+	return &processingServiceClient{cc}
 }
 
-func (c *fileServiceClient) ProcessFile(ctx context.Context, in *FileRequest, opts ...grpc.CallOption) (*FileResponse, error) {
+func (c *processingServiceClient) ProcessFile(ctx context.Context, in *ProcessRequest, opts ...grpc.CallOption) (*ProcessResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(FileResponse)
-	err := c.cc.Invoke(ctx, FileService_ProcessFile_FullMethodName, in, out, cOpts...)
+	out := new(ProcessResponse)
+	err := c.cc.Invoke(ctx, ProcessingService_ProcessFile_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// FileServiceServer is the server API for FileService service.
-// All implementations must embed UnimplementedFileServiceServer
+// ProcessingServiceServer is the server API for ProcessingService service.
+// All implementations must embed UnimplementedProcessingServiceServer
 // for forward compatibility.
-type FileServiceServer interface {
-	ProcessFile(context.Context, *FileRequest) (*FileResponse, error)
-	mustEmbedUnimplementedFileServiceServer()
+type ProcessingServiceServer interface {
+	ProcessFile(context.Context, *ProcessRequest) (*ProcessResponse, error)
+	mustEmbedUnimplementedProcessingServiceServer()
 }
 
-// UnimplementedFileServiceServer must be embedded to have
+// UnimplementedProcessingServiceServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedFileServiceServer struct{}
+type UnimplementedProcessingServiceServer struct{}
 
-func (UnimplementedFileServiceServer) ProcessFile(context.Context, *FileRequest) (*FileResponse, error) {
+func (UnimplementedProcessingServiceServer) ProcessFile(context.Context, *ProcessRequest) (*ProcessResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ProcessFile not implemented")
 }
-func (UnimplementedFileServiceServer) mustEmbedUnimplementedFileServiceServer() {}
-func (UnimplementedFileServiceServer) testEmbeddedByValue()                     {}
+func (UnimplementedProcessingServiceServer) mustEmbedUnimplementedProcessingServiceServer() {}
+func (UnimplementedProcessingServiceServer) testEmbeddedByValue()                           {}
 
-// UnsafeFileServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to FileServiceServer will
+// UnsafeProcessingServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ProcessingServiceServer will
 // result in compilation errors.
-type UnsafeFileServiceServer interface {
-	mustEmbedUnimplementedFileServiceServer()
+type UnsafeProcessingServiceServer interface {
+	mustEmbedUnimplementedProcessingServiceServer()
 }
 
-func RegisterFileServiceServer(s grpc.ServiceRegistrar, srv FileServiceServer) {
-	// If the following call pancis, it indicates UnimplementedFileServiceServer was
+func RegisterProcessingServiceServer(s grpc.ServiceRegistrar, srv ProcessingServiceServer) {
+	// If the following call pancis, it indicates UnimplementedProcessingServiceServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&FileService_ServiceDesc, srv)
+	s.RegisterService(&ProcessingService_ServiceDesc, srv)
 }
 
-func _FileService_ProcessFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(FileRequest)
+func _ProcessingService_ProcessFile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProcessRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FileServiceServer).ProcessFile(ctx, in)
+		return srv.(ProcessingServiceServer).ProcessFile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FileService_ProcessFile_FullMethodName,
+		FullMethod: ProcessingService_ProcessFile_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FileServiceServer).ProcessFile(ctx, req.(*FileRequest))
+		return srv.(ProcessingServiceServer).ProcessFile(ctx, req.(*ProcessRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// FileService_ServiceDesc is the grpc.ServiceDesc for FileService service.
+// ProcessingService_ServiceDesc is the grpc.ServiceDesc for ProcessingService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var FileService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "fileservice.FileService",
-	HandlerType: (*FileServiceServer)(nil),
+var ProcessingService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "fileservice.ProcessingService",
+	HandlerType: (*ProcessingServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "ProcessFile",
-			Handler:    _FileService_ProcessFile_Handler,
+			Handler:    _ProcessingService_ProcessFile_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
